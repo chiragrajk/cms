1c2e8f23f95e88f244512e67dc6dd43f4546f0ff
Chirag Rajkarnikar <chiragrajk@gmail.com>
2018-11-15 19:59:13 +0100

    update configs for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,11 +11,19 @@ use Mix.Config
 # before starting your production server.
 config :cms, CmsWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "elixir-cms.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :cms, Hello.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -68,4 +76,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
--- lib/cms_web/endpoint.ex
+++ lib/cms_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule CmsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :cms
 
   socket "/socket", CmsWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
