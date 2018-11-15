adc2b785e10801850ded9031ceee70f1a56dde6b
Chirag Rajkarnikar <chiragrajk@gmail.com>
2018-11-15 17:45:49 +0100

    set up bootstrap

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,7 +31,18 @@ module.exports = (env, options) => ({
       {
         test: /\.s?css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
+      },
+      // {
+      //   test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      //   use: [{
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[ext]',
+      //       outputPath: 'fonts/',    // where the fonts will go
+      //       // publicPath: '../'       // override the default path
+      //     }
+      //   }]
+      // }
     ]
   },
   plugins: [
